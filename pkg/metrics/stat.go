0a7ef905ae7471001f82e163cd0bd79477eb37c9
potter <potter@d-robotics.cc>
2020-12-14 10:27:13 +0800

    增加统计

--- pkg/metrics/stat.go
+++ pkg/metrics/stat.go
@@ -8,18 +8,25 @@ import (
 var (
 	SyncAllDurationsHistogram = prometheus.NewHistogram(
 		prometheus.HistogramOpts{
-			Name: "sync_all_durations_histogram",
-			Help: "",
-			Buckets: prometheus.LinearBuckets(0.0,1000,10),
+			Name:    "sync_all_durations_histogram",
+			Help:    "",
+			Buckets: prometheus.LinearBuckets(0.0, 1000, 10),
 		})
 	SyncOnceDurationsHistogram = prometheus.NewHistogram(
 		prometheus.HistogramOpts{
-			Name: "sync_once_durations_histogram",
-			Help: "",
-			Buckets: prometheus.LinearBuckets(0.0,1000,10),
+			Name:    "sync_once_durations_histogram",
+			Help:    "",
+			Buckets: prometheus.LinearBuckets(0.0, 1000, 10),
 		})
+	SyncOnceGauge = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "sync_once_gauge",
+			Help: "",
+		},
+		[]string{"syncgauge"},
+	)
 )
 
 func init() {
-	prometheus.MustRegister(SyncAllDurationsHistogram,SyncOnceDurationsHistogram)
+	prometheus.MustRegister(SyncAllDurationsHistogram, SyncOnceDurationsHistogram, SyncOnceGauge)
 }
